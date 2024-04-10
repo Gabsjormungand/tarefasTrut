c0c068a0e7117fbda3201c631b14913d06f30686
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-04-09 22:20:30 -0300

    Update

--- index.php
+++ index.php
@@ -1,6 +1,49 @@
 <?php include "cabecalho.php" ?>
 
 <h1>📌Tarefas</h1>
-<a href="#" class="btn btn-primary">Adicionar Tarefa</a>
+<a href="novo-formulario.php" class="btn btn-primary">Adicionar Tarefa</a>
+
+<table class="table">
+    <tr>
+        <th>&nbsp;</th>
+        <th>Id</th>
+        <th>Título</th>
+        <th>Descrição</th>
+        <th>Status</th>
+    </tr>
+    <?php
+    include "conexao.php";
+    $sql = "select * from tarefas order by status asc, id desc";
+    $resultado = mysqli_query($conexao, $sql);
+
+    while($umaTarefa = mysqli_fetch_assoc($resultado)):
+    ?>
+        <tr>
+            <td>
+                <?php
+                if($umaTarefa['status'] == 0){
+                    ?>
+                        <a href="editar-salvar.php" class="btn">✔</a>
+                    <?php
+                }
+                ?>
+            </td>
+            <td><?=$umaTarefa['id'];?></td>
+            <td><?=$umaTarefa['titulo'];?></td>
+            <td><?=$umaTarefa['descricao'];?></td>
+            <td>
+                <?php
+                if($umaTarefa['status'] == 1){
+                    echo "Completo";
+                }else{
+                    echo "Pendente";
+                }
+                ?>
+            </td>
+        </tr>
+    <?php
+    endwhile;
+    ?>
+</table>
 
 <?php include "rodape.php" ?>
